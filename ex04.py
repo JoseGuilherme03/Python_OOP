92bb9f70e7c47a868b6bc679690710d54c6f3d1e
José Guilherme <josegfernandes03@gmail.com>
2023-02-28 11:04:17 -0300

    atividade pratica 04 finalizada

---
+++ ex04.py
@@ -0,0 +1,12 @@
+class Usuario():
+    def __init__(self, primeiro_nome, ultimo_nome):
+        self.__primeiro_nome = primeiro_nome
+        self.__ultimo_nome = ultimo_nome
+
+    def __str__(self):
+        return f"Nome Completo: {self.__primeiro_nome} {self.__ultimo_nome}"
+
+
+print(Usuario("José", "Fernandes"))
+
+    
