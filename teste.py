fdceacc8dd7a3272a9406e7f376c2af58cb1feb8
José Guilherme <josegfernandes03@gmail.com>
2023-05-21 20:43:19 -0300

    Parte 2 do Grande Softaware

--- teste.py
+++ teste.py
@@ -1,5 +1,4 @@
 from enum import Enum
-from tabulate import tabulate
 
 
 class Builder(Enum):
@@ -55,19 +54,7 @@ class Guitar:
         return self._spec
 
     def __str__(self):
-        data = [
-            ["Serial Number", self.serial_number],
-            ["Price", self.price],
-            ["Builder", self.spec.builder.value],
-            ["Model", self.spec.model],
-            ["Type", self.spec.typeg.value],
-            ["Back Wood", self.spec.back_wood.value],
-            ["Top Wood", self.spec.top_wood.value],
-        ]
-
-        table = tabulate(data, headers=["Attribute", "Value"], tablefmt="fancy_grid")
-
-        return table
+        return f"Serial Number: {self.serial_number}, Price: {self.price}, Builder: {self.spec.builder.value}, Model: {self.spec.model}, Type: {self.spec.typeg.value}, Back Wood: {self.spec.back_wood.value}, Top Wood: {self.spec.top_wood.value}"
 
 
 class GuitarSpec:
@@ -108,36 +95,36 @@ class Inventory:
             self.guitars.append(guitar)
 
     def get_guitar(self, serial_number):
-        if self.guitars:
-            for guitar in self.guitars:
-                if guitar.serial_number == serial_number:
-                    return guitar
-        return "Guitarra não encontrada"
+        for guitar in self.guitars:
+            if guitar.serial_number == serial_number:
+                print(guitar)
+            else:
+                print("Nenhum guitarra encontrada com esse serial number")
 
     def search_guitar(self, search_guitar):
         list_guitar = []
         if self.guitars:
             for guitar in self.guitars:
-                if search_guitar.builder != guitar.spec.builder:
+                if search_guitar.builder != guitar.builder:
                     continue
 
                 model = search_guitar.model.lower()
-                if model != "" and model != guitar.spec.model.lower():
+                if model != "" and model != guitar.model.lower():
                     continue
 
-                if search_guitar.typeg != guitar.spec.typeg:
+                if search_guitar.typeg != guitar.typeg:
                     continue
 
-                if search_guitar.back_wood != guitar.spec.back_wood:
+                if search_guitar.back_wood != guitar.back_wood:
                     continue
 
-                if search_guitar.top_wood != guitar.spec.top_wood:
+                if search_guitar.top_wood != guitar.top_wood:
                     continue
 
                 list_guitar.append(guitar)
 
         if list_guitar:
-            print("\033[32mGuitarra(s) encontrada(s): \033[0m")
+            print("Guitarra(s) encontrada(s): ")
             for guitar in list_guitar:
                 print(guitar)
                 print()
@@ -145,10 +132,7 @@ class Inventory:
         else:
             print("Nenhuma guitarra encontrada")
 
-
 inventory = Inventory()
-
-# Adicionando guitarras no inventário
 inventory.add_guitar(
     Guitar(
         serial_number="1",
@@ -185,28 +169,5 @@ inventory.add_guitar(
     ),
 )
 
-# Pesquisando por uma guitarra específica
-guitar_erin_like1 = GuitarSpec(
-    typeg=TypeG.ELETRIC,
-    back_wood=Wood.ALDER,
-    top_wood=Wood.ALDER,
-    builder=Builder.FENDER,
-    model="Stratocastor",
-)
-
-guitar_joe_like1 = GuitarSpec(
-    typeg=TypeG.ELETRIC,
-    back_wood=Wood.BRAZILIAN_ROSEWOOD,
-    top_wood=Wood.BRAZILIAN_ROSEWOOD,
-    builder=Builder.FENDER,
-    model="Stratocastor",
-)
-
-# Pesquisando por guitarras que o Erin gosta
-inventory.search_guitar(guitar_erin_like1)
-
-
-# Outros testes #
-# print(inventory.get_guitar("2"))
-# print(inventory.get_guitar("4"))
-# inventory.search_guitar(guitar_joe_like1)
+for guitar in inventory.guitars:
+    print(guitar)
