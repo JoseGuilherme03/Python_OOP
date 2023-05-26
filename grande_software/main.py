ff710c7ec7bae6213aa6412094eee9f0e5be9b03
José Guilherme <josegfernandes03@gmail.com>
2023-05-26 09:47:16 -0300

    grande software versão 3

--- grande_software/main.py
+++ grande_software/main.py
@@ -71,12 +71,13 @@ class Guitar:
 
 
 class GuitarSpec:
-    def __init__(self, typeg, back_wood, top_wood, builder, model):
+    def __init__(self, typeg, back_wood, top_wood, builder, model, num_strings):
         self._typeg = typeg
         self._back_wood = back_wood
         self._top_wood = top_wood
         self._builder = builder
         self._model = model
+        self._num_strings = num_strings
 
     @property
     def typeg(self):
@@ -98,6 +99,25 @@ class GuitarSpec:
     def model(self):
         return self._model
 
+    @property
+    def num_strings(self):
+        return self._num_strings
+
+    def matches(self, otherspec):
+        if self.builder != otherspec.builder:
+            return False
+        if self.model and self.model.lower() != otherspec.model.lower():
+            return False
+        if self.typeg != otherspec.typeg:
+            return False
+        if self.back_wood != otherspec.back_wood:
+            return False
+        if self.top_wood != otherspec.top_wood:
+            return False
+        if self.num_strings != otherspec.num_strings:
+            return False
+        return True
+
 
 class Inventory:
     def __init__(self):
@@ -114,99 +134,76 @@ class Inventory:
                     return guitar
         return "Guitarra não encontrada"
 
-    def search_guitar(self, search_guitar):
-        list_guitar = []
-        if self.guitars:
-            for guitar in self.guitars:
-                if search_guitar.builder != guitar.spec.builder:
-                    continue
-
-                model = search_guitar.model.lower()
-                if model != "" and model != guitar.spec.model.lower():
-                    continue
-
-                if search_guitar.typeg != guitar.spec.typeg:
-                    continue
-
-                if search_guitar.back_wood != guitar.spec.back_wood:
-                    continue
-
-                if search_guitar.top_wood != guitar.spec.top_wood:
-                    continue
-
-                list_guitar.append(guitar)
-
-        if list_guitar:
-            print("\033[32mGuitarra(s) encontrada(s): \033[0m")
-            for guitar in list_guitar:
-                print(guitar)
-                print()
-
-        else:
-            print("Nenhuma guitarra encontrada")
+    def search(self, searchguitar):
+        matching_guitars = []
+        for guitar in self.guitars:
+            if guitar.spec.matches(searchguitar):
+                matching_guitars.append(guitar)
+        return matching_guitars
 
 
-inventory = Inventory()
+def main():
+    inventory = Inventory()
 
-# Adicionando guitarras no inventário
-inventory.add_guitar(
-    Guitar(
-        serial_number="1",
-        price=1499.95,
-        spec=GuitarSpec(
-            typeg=TypeG.ELETRIC,
-            back_wood=Wood.ALDER,
-            top_wood=Wood.ALDER,
-            builder=Builder.FENDER,
-            model="Stratocastor",
+    # Adicionando guitarras no inventário
+    inventory.add_guitar(
+        Guitar(
+            serial_number="1",
+            price=1499.95,
+            spec=GuitarSpec(
+                typeg=TypeG.ELETRIC,
+                back_wood=Wood.ALDER,
+                top_wood=Wood.ALDER,
+                builder=Builder.FENDER,
+                model="Stratocastor",
+                num_strings=6
+            ),
         ),
-    ),
-    Guitar(
-        serial_number="2",
-        price=1549.95,
-        spec=GuitarSpec(
-            typeg=TypeG.ELETRIC,
-            back_wood=Wood.ALDER,
-            top_wood=Wood.ALDER,
-            builder=Builder.FENDER,
-            model="Stratocastor",
+        Guitar(
+            serial_number="2",
+            price=1549.95,
+            spec=GuitarSpec(
+                typeg=TypeG.ELETRIC,
+                back_wood=Wood.ALDER,
+                top_wood=Wood.ALDER,
+                builder=Builder.FENDER,
+                model="Stratocastor",
+                num_strings=12
+            ),
         ),
-    ),
-    Guitar(
-        serial_number="3",
-        price=1899.99,
-        spec=GuitarSpec(
-            typeg=TypeG.ELETRIC,
-            back_wood=Wood.BRAZILIAN_ROSEWOOD,
-            top_wood=Wood.BRAZILIAN_ROSEWOOD,
-            builder=Builder.FENDER,
-            model="Stratocastor",
-        ),
-    ),
-)
+        Guitar(
+            serial_number="3",
+            price=549.95,
+            spec=GuitarSpec(
+                typeg=TypeG.ACOUSTIC,
+                back_wood=Wood.ALDER,
+                top_wood=Wood.ALDER,
+                builder=Builder.FENDER,
+                model="Stratocastor",
+                num_strings=6
+            ),
+        )
+    )
 
-# Pesquisando por uma guitarra específica
-guitar_erin_like1 = GuitarSpec(
-    typeg=TypeG.ELETRIC,
-    back_wood=Wood.ALDER,
-    top_wood=Wood.ALDER,
-    builder=Builder.FENDER,
-    model="Stratocastor",
-)
+    # Guitarra que o cliente está procurando
+    guitar_erin_like1 = GuitarSpec(
+        typeg=TypeG.ELETRIC,
+        back_wood=Wood.ALDER,
+        top_wood=Wood.ALDER,
+        builder=Builder.FENDER,
+        model="Stratocastor",
+        num_strings=6,
+    )
 
-guitar_joe_like1 = GuitarSpec(
-    typeg=TypeG.ELETRIC,
-    back_wood=Wood.BRAZILIAN_ROSEWOOD,
-    top_wood=Wood.BRAZILIAN_ROSEWOOD,
-    builder=Builder.FENDER,
-    model="Stratocastor",
-)
+    matchingGuitars = inventory.search(guitar_erin_like1)
 
-# Pesquisando por guitarras que o Erin gosta
-inventory.search_guitar(guitar_erin_like1)
+    if matchingGuitars:
+        print("Erin, talvez você goste destas: ")
+        for guitar in matchingGuitars:
+            print(guitar)
+    else:
+        print("Desculpe Erin, não temos nada para você")
 
 
-# Outros testes #
-# print(inventory.get_guitar("2"))
-# print(inventory.get_guitar("4"))
-# inventory.search_guitar(guitar_joe_like1)
+if __name__ == "__main__":
+    main()
